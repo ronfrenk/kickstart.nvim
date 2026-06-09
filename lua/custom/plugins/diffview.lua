f1d93ba05989ed1be8379fe1869e3fb09de642b9
Ron Frenkel <ron.frenkel@melio.com>
2026-06-09 17:24:18 +0300

    improve diffview key mappings

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -2,7 +2,8 @@ return {
   'sindrets/diffview.nvim',
   cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
   keys = {
-    { '<leader>gl', '<cmd>DiffviewOpen HEAD~1<cr>', desc = '[G]it [L]ast commit diff' },
-    { '<leader>gL', '<cmd>DiffviewClose<cr>', desc = '[G]it [L]ast commit diff close' },
+    { '<leader>gdh', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff [H]ead changes' },
+    { '<leader>gdl', '<cmd>DiffviewOpen HEAD~1<cr>', desc = '[G]it [D]iff [L]ast commit diff' },
+    { '<leader>gdc', '<cmd>DiffviewClose<cr>', desc = '[G]it [D]iff [C]lose' },
   },
 }

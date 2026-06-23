b17f6a237aa9b3317ac3c070c7cc291b8c0cdb86
Ron Frenkel <ron.frenkel@melio.com>
2026-06-23 16:16:56 +0300

    fix gitdiff main - only show commits from branch

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -4,7 +4,7 @@ return {
   keys = {
     { '<leader>gdh', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff [H]ead changes' },
     { '<leader>gdl', '<cmd>DiffviewOpen HEAD~1<cr>', desc = '[G]it [D]iff [L]ast commit diff' },
-    { '<leader>gdm', '<cmd>DiffviewOpen main<cr>', desc = '[G]it [D]iff [m]ain' },
+    { '<leader>gdm', '<cmd>DiffviewOpen main...HEAD<cr>', desc = '[G]it [D]iff [m]ain (branch changes only)' },
     { '<leader>gdc', '<cmd>DiffviewClose<cr>', desc = '[G]it [D]iff [C]lose' },
   },
 }
